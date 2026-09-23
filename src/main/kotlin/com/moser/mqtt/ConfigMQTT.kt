1f31346d13f94540b7058703b0519dbb3131be69
Corentin Bouyer <corentin.bouyer@lyceeastier.com>
2026-09-23 22:06:50 +0200

    Use environment variable for MQTT password
    Replaced hardcoded PASSWORD with environment variable TTN_API_KEY.

--- src/main/kotlin/com/moser/mqtt/ConfigMQTT.kt
+++ src/main/kotlin/com/moser/mqtt/ConfigMQTT.kt
@@ -4,5 +4,5 @@ package com.moser.mqtt
 const val SERVER_URI = "eu1.cloud.thethings.network";
 const val PORT = 8883;
 const val USERNAME = "projet-enerdis-2025@ttn";
-val PASSWORD = "NNSXS.YXGFJIHYQ4KAUB55RUFWUAYRSOWM35Y6MVS7KPI.WO7OEH3PUOCBIOZYEVKGBGSQ4OBPN3ICBNXTS3PDXYWD6SDEGEGA".toByteArray();
+val PASSWORD = (System.getenv("TTN_API_KEY") ?: error("Variable TTN_API_KEY manquante")).toByteArray()
 const val TOPIC = "#";
